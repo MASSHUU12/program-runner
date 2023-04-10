a75ac5d9e911f27051a991db799dbb26d2d1e46e
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-04-10 18:31:05 +0200

    Console management via Spectre.Console

--- src/commands/RunCommand.cs
+++ src/commands/RunCommand.cs
@@ -1,4 +1,6 @@
+using Spectre.Console;
 using Spectre.Console.Cli;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 
 public class RunCommand : Command<RunCommand.Settings>
@@ -12,12 +14,38 @@ public class RunCommand : Command<RunCommand.Settings>
     public string? ListName { get; set; }
 
     [CommandOption("-l|--log")]
+    [DefaultValue("all")]
     public string? Log { get; set; }
+  }
+
+  public override ValidationResult Validate([NotNull] CommandContext context, [NotNull] Settings settings)
+  {
+    if (!File.Exists(settings.List))
+      return ValidationResult.Error($"File does not exists: {settings.List}");
+
+    if (!new[] { "all", "off", "error" }.Contains(settings.Log))
+      return ValidationResult.Error(
+        $"Log option does not accept: {settings.Log}\nPossible values are: all, off, error."
+        );
+
+    return base.Validate(context, settings);
   }
 
   public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
   {
-    Console.WriteLine($"{settings.List} {settings.ListName ?? "main"} {settings.Log}");
+    // Set log level
+    Log.severity = settings.Log ?? "main";
+
+    Func<string> checkListName = () =>
+    {
+      if (settings.ListName != null)
+        return settings.ListName;
+
+      Log.Warning("No list was passed to run, running the default \"main\".");
+      return "main";
+    };
+
+    Runner.Prepare(settings.List ?? "", checkListName());
 
     return 0;
   }
